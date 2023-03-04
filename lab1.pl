8735c4fc7ca2d5cb1774bc1eb84120a13a891b4d
Sajid Majeed <92437858+SajidMajeed92@users.noreply.github.com>
2023-03-04 23:03:17 +0500

    lab Prolog file

---
+++ lab1.pl
@@ -0,0 +1,39 @@
+% lab conatining task related to fact(Properties) & fact(Relationships)
+
+% fact Property woman
+
+woman(saima).
+woman(ayesha).
+woman(aleena).
+woman(sara).
+woman(aiman).
+
+% fact property man
+man(ahmed).
+man(bilal).
+man(huzaifa).
+
+% fact property walk
+walk(farhan).
+
+% fact property happy
+happy(ali).
+
+%fact property sad
+sad(ahsan).
+
+% fact property rich
+rich(arbaz).
+
+% fact (Relationship)
+father(arsalan,saima). % Arsalan is a father of Saima
+father(ayesha,saima). % Ayesha is a mother of saima
+drive(farhan,car). % Farhan drives Car
+likes(ali,icecream). % Ali Likes Ice Cream
+friend(ali,farhan). % Ali is a friend of farhan
+play(ali,cricket).% Ali plays cricket
+
+% Fact
+little(john). % John is little
+forgot(ali,raincoat). % Ali forgot his raincoat
+lost(usama,keys). % usama lost his keys
